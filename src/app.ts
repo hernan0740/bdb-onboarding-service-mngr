f6ef641acf30944be0c89cbe9df0921eba8f1241
hchapid <hchapid@bancodebogota.com.co>
2025-05-31 11:03:00 -0500

    clear main branch

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import employeeRoutes from './routes/employeeRoutes';
-
-const app = express();
-app.use(express.json());
-app.use('/employees', employeeRoutes);
-
-export default app;
--- src/routes/employeeRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getEmployees, addEmployee } from '../controllers/employeeController';
-
-const router = Router();
-
-router.get('/', getEmployees);
-router.post('/', addEmployee);
-
-export default router;
