16a3e253e23590c564b1fea9821603042622b3b7
hchapid <hchapid@bancodebogota.com.co>
2025-06-01 15:12:16 -0500

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import employeeRoutes from './routes/employeeRoutes';
+import cors from 'cors';
 
 
 const port = 5000;
 const app = express();
 
+app.use(cors());
 
 app.use(express.json());
 app.use('/employees', employeeRoutes);
