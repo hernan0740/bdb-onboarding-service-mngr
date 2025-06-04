9049fce7a0761516b9ae6fa6ece1cd2a19c8583d
hchapid <hchapid@bancodebogota.com.co>
2025-06-04 13:29:16 -0500

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://frontend-kata-hchapid.s3-website-us-east-1.amazonaws.com"],
+    origin:'*',
     credentials: true,
   }),
 );
